5f96546f235ad6d94e20d7917ada747c7088cf35
Joseph Walker <josephwalker@tred.com>
2018-09-02 21:59:21 -0500

    In the right spot this time.

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-printf '\e[1;32m%-6s\e[m' "Deploying:\n"
-printf '\e[1;32m%-6s\e[m' "Deleting branch 'gh-pages'...\n"
+printf '\e[1;32m%-6s\e[m\n' "Deploying:"
+printf '\e[1;32m%-6s\e[m\n' "Deleting branch 'gh-pages'..."
 git branch -D gh-pages
 
-printf '\e[1;32m%-6s\e[m' "Creating new deployment branch...\n"
+printf '\e[1;32m%-6s\e[m\n' "Creating new deployment branch..."
 git checkout -b gh-pages
 
-printf '\e[1;32m%-6s\e[m' "Shuffling indexes...\n"
+printf '\e[1;32m%-6s\e[m\n' "Shuffling indexes..."
 mv ./index.html ./index-dev.html
 mv ./index-prod.html ./index.html
 
-printf '\e[1;32m%-6s\e[m' "Running build...\n"
+printf '\e[1;32m%-6s\e[m\n' "Running build..."
 npm run build-prod
 
-printf '\e[1;32m%-6s\e[m' "Commiting build assets...\n"
+printf '\e[1;32m%-6s\e[m\n' "Commiting build assets..."
 git add -A
 git add -f --all dist/
 git commit -m "Automated Deployment"
 
-printf '\e[1;32m%-6s\e[m' "Deploying to Github Pages...\n"
+printf '\e[1;32m%-6s\e[m\n' "Deploying to Github Pages..."
 git push -f origin gh-pages
 
-printf '\e[1;32m%-6s\e[m' "Deployment complete. Cleaning up...\n"
+printf '\e[1;32m%-6s\e[m\n' "Deployment complete. Cleaning up..."
 mv ./index.html ./index-prod.html
 mv ./index-dev.html ./index.html
 git checkout -f master
