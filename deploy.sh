ac94fb6dc950746c283237ddaab1623175332b7a
Joseph Walker <josephwalker@tred.com>
2018-09-02 21:58:15 -0500

    Pretty colors

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-echo "Deploying:"
-echo "Deleting branch 'gh-pages'..."
+printf '\e[1;32m%-6s\e[m' "Deploying:"
+printf '\e[1;32m%-6s\e[m' "Deleting branch 'gh-pages'..."
 git branch -D gh-pages
 
-echo "Creating new deployment branch..."
+printf '\e[1;32m%-6s\e[m' "Creating new deployment branch..."
 git checkout -b gh-pages
 
-echo "Shuffling indexes..."
+printf '\e[1;32m%-6s\e[m' "Shuffling indexes..."
 mv ./index.html ./index-dev.html
 mv ./index-prod.html ./index.html
 
-echo "Running build..."
+printf '\e[1;32m%-6s\e[m' "Running build..."
 npm run build-prod
 
-echo "Commiting build assets..."
+printf '\e[1;32m%-6s\e[m' "Commiting build assets..."
 git add -A
 git add -f --all dist/
 git commit -m "Automated Deployment"
 
-echo "Deploying to Github Pages..."
+printf '\e[1;32m%-6s\e[m' "Deploying to Github Pages..."
 git push -f origin gh-pages
 
-echo "Deployment complete. Cleaning up..."
+printf '\e[1;32m%-6s\e[m' "Deployment complete. Cleaning up..."
 mv ./index.html ./index-prod.html
 mv ./index-dev.html ./index.html
 git checkout -f master
